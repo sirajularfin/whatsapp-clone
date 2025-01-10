0664445914d317568d602fb7ef5f7daa9e91f14c
Sirajul Arfin <sirajularfin@gmail.com>
2025-01-10 17:45:28 +0530

    update app.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Dashboard from './screens/dashboard';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Dashboard}
+          options={{title: 'Welcome'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
