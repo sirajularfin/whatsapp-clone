17c993366c7e067bf8f94849c4d62055c4a03661
Sirajul Arfin <sirajularfin@gmail.com>
2025-02-16 00:33:23 +0530

    feat: add recent chat component

---
+++ src/components/recent-chat/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Text from '../text';
+import { FontVariant } from '../text/styles';
+import ScaledSize from '../../themes/sizes';
+import Container from '../container';
+import Image from '../image';
+import Color from '../../themes/colors';
+
+interface IProps {
+  lastChatDateTime?: string;
+  lastChatMessage?: string;
+  personName?: string;
+  profileImageUrl?: string;
+}
+
+const RecentChat: React.FC<IProps> = ({ ...props }) => {
+  return (
+    <Container
+      alignItems="center"
+      backgroundColor={Color.GREY_50}
+      borderBottomWidth={ScaledSize.XS_1}
+      borderColor={Color.GREY_200}
+      borderRadius={ScaledSize.XS_10}
+      flexDirection="row"
+      gap={ScaledSize.XS_10}
+      marginHorizontal={ScaledSize.XS_10}
+      marginVertical={ScaledSize.XS_5}
+      padding={ScaledSize.XS_10}
+    >
+      <Image
+        source={props.profileImageUrl}
+        style={{
+          borderRadius: ScaledSize.M_25,
+          height: ScaledSize.XL_50,
+          width: ScaledSize.XL_50,
+        }}
+      />
+      <Container flex={1}>
+        <Container
+          alignItems="center"
+          flexDirection="row"
+          justifyContent="space-between"
+        >
+          <Text variant={FontVariant.HeadingSmall}>{props.personName}</Text>
+          <Text variant={FontVariant.LabelSmall}>{props.lastChatDateTime}</Text>
+        </Container>
+        <Text variant={FontVariant.BodyMedium} numberOfLines={1}>
+          {props.lastChatMessage}
+        </Text>
+      </Container>
+    </Container>
+  );
+};
+
+export default RecentChat;
