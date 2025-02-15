95a7f80daa50314cdca4496d2cb927998a314b8e
Sirajul Arfin <sirajularfin@gmail.com>
2025-02-16 01:40:54 +0530

    update: home screen

--- src/screens/home-screen/index.tsx
+++ src/screens/home-screen/index.tsx
@@ -6,24 +6,31 @@ import data from '../../mocks/chatHistory.json';
 import Text from '../../components/text';
 import { FontVariant } from '../../components/text/styles';
 import ScreenLayout from '../../components/screen-layout';
+import Container from '../../components/container';
+import ScaledSize from '../../themes/sizes';
+
+const flatListSeparator = () => <Container height={ScaledSize.XS_5} />;
 
 const HomeScreen: React.FC = () => {
   return (
     <ScreenLayout backgroundColor={Color.WHITE}>
-      <Text variant={FontVariant.HeadingSmall} textAlign="center">
-        BuzzChat
-      </Text>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => (
-          <RecentChat
-            personName={item.name}
-            profileImageUrl={item.profileImageUrl}
-            lastChatMessage={item.message}
-            lastChatDateTime={item.lastChatDateTime}
-          />
-        )}
-      />
+      <Container rowGap={ScaledSize.S_20}>
+        <Text variant={FontVariant.HeadingSmall} textAlign="center">
+          BuzzChat
+        </Text>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <RecentChat
+              personName={item.name}
+              profileImageUrl={item.profileImageUrl}
+              lastChatMessage={item.message}
+              lastChatDateTime={item.lastChatDateTime}
+            />
+          )}
+          ItemSeparatorComponent={flatListSeparator}
+        />
+      </Container>
     </ScreenLayout>
   );
 };
