18deebfccd110ab7a33afaeea71961d8065d34c3
Sirajul Arfin <sirajularfin@gmail.com>
2025-02-16 01:14:32 +0530

    update: text component

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,24 +1,31 @@
 import React, { PropsWithChildren } from 'react';
-import { Text as NativeText } from 'react-native';
+import { Text as NativeText, TextStyle } from 'react-native';
 import { FontVariant, styles } from './styles';
 
-interface IProps {
-  numberOfLines?: number;
-  useLetterSpacing?: boolean;
-  useLineHeight?: boolean;
-  variant: FontVariant;
-}
+type IProps = Partial<
+  Pick<TextStyle, 'textAlign'> & {
+    numberOfLines: number;
+    useLetterSpacing: boolean;
+    useLineHeight: boolean;
+    variant: FontVariant;
+  }
+>;
 
 const Text: React.FC<PropsWithChildren<IProps>> = ({
   children,
-  numberOfLines,
   useLetterSpacing = true,
   useLineHeight = true,
-  variant,
+  ...props
 }) => {
   const classes = styles(useLineHeight, useLetterSpacing);
   return (
-    <NativeText style={classes[variant]} numberOfLines={numberOfLines}>
+    <NativeText
+      style={[
+        { textAlign: props.textAlign },
+        classes[props.variant as keyof typeof classes],
+      ]}
+      numberOfLines={props.numberOfLines}
+    >
       {children}
     </NativeText>
   );
