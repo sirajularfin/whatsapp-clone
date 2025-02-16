6342d13621894bb87b01704fef5f90f7e5686d44
Sirajul Arfin <sirajularfin@gmail.com>
2025-02-16 18:49:31 +0530

    fix: move header title to screen layout

--- src/components/screen-layout/index.tsx
+++ src/components/screen-layout/index.tsx
@@ -2,9 +2,14 @@ import React, { PropsWithChildren } from 'react';
 import Color from '../../themes/colors';
 import { StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Text from '../text';
+import { FontVariant } from '../text/styles';
+import Container from '../container';
+import ScaledSize from '../../themes/sizes';
 
 interface IProps {
   backgroundColor?: Color;
+  headerTitle?: string;
 }
 
 const ScreenLayout: React.FC<PropsWithChildren<IProps>> = ({
@@ -23,6 +28,16 @@ const ScreenLayout: React.FC<PropsWithChildren<IProps>> = ({
         },
       ]}
     >
+      <Container marginBottom={ScaledSize.S_20}>
+        <Text
+          color={Color.BLACK}
+          textAlign="center"
+          useLineHeight={false}
+          variant={FontVariant.HeadingSmall}
+        >
+          {props.headerTitle}
+        </Text>
+      </Container>
       {children}
     </View>
   );
--- src/screens/home-screen/index.tsx
+++ src/screens/home-screen/index.tsx
@@ -3,8 +3,6 @@ import { FlatList } from 'react-native';
 import RecentChat from '../../components/recent-chat';
 import Color from '../../themes/colors';
 import data from '../../mocks/chatHistory.json';
-import Text from '../../components/text';
-import { FontVariant } from '../../components/text/styles';
 import ScreenLayout from '../../components/screen-layout';
 import Container from '../../components/container';
 import ScaledSize from '../../themes/sizes';
@@ -15,16 +13,8 @@ const flatListSeparator = () => <Container height={ScaledSize.XS_5} />;
 
 const HomeScreen: React.FC = () => {
   return (
-    <ScreenLayout backgroundColor={Color.WHITE}>
+    <ScreenLayout backgroundColor={Color.WHITE} headerTitle="BuzzChat">
       <Container rowGap={ScaledSize.S_20} paddingHorizontal={ScaledSize.XS_10}>
-        <Text
-          color={Color.BLACK}
-          textAlign="center"
-          useLineHeight={false}
-          variant={FontVariant.HeadingSmall}
-        >
-          BuzzChat
-        </Text>
         <TextInput
           placeholder="Search"
           icon={<SearchIcon height={ScaledSize.S_15} width={ScaledSize.S_15} />}
