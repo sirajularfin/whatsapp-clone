00c4d64d6421a7ac1f81eb8bee0a7277b4dd3c4c
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-04 15:14:32 +0530

    feat: add util function to formatJson

--- src/util/commons.util.ts
+++ src/util/commons.util.ts
@@ -1,5 +1,13 @@
 import { Platform } from 'react-native';
 
-export const isIOS = () => {
+export function isIOS() {
   return Platform.OS === 'ios';
-};
+}
+
+export function formatJson(args: string) {
+  try {
+    return JSON.stringify(JSON.parse(args), null, 2);
+  } catch (e) {
+    return args;
+  }
+}
